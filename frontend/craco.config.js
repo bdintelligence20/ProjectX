e9497d8b2208042a0680afb6ed4a05032495e361
bdintelligence20 <nicholasf@lrmg.co.za>
2024-11-11 14:25:48 +0000

    craco build

---
+++ frontend/craco.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+  eslint: {
+    enable: false,
+  },
+  babel: {
+    presets: [],
+    plugins: [],
+  },
+};
