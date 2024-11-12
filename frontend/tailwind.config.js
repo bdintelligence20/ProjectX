4569abf42d3a81f29078f776d2d85a9b10c6f530
bdintelligence20 <nicholasf@lrmg.co.za>
2024-11-12 06:57:26 +0000

    tailwind errors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,6 +3,8 @@ module.exports = {
   darkMode: ["class"],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
